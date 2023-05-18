1db625e8744b0a3ac927a1fe3f415c760a1e84d7
jinss5 <jinss1145@gmail.com>
2023-05-18 16:57:35 +0900

    add: signin

--- Backend/git-rebase-test/app.js
+++ Backend/git-rebase-test/app.js
@@ -31,6 +31,21 @@ app.get("/ping", (req, res) => {
 feature/signin 브랜치의 경우 app.post('/users/signin', ...)
 feature/signup 브랜치의 경우 app.post('/users/signup', ...)
 */
+app.post("/users/signin", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await appDataSource.query(
+    `
+    SELECT
+      users.id
+      users.password
+    FROM
+      users
+    WHERE
+      users.email = ?
+  `,
+    [email]
+  );
+});
 
 app.listen(PORT, () => {
   appDataSource
